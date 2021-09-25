1a3dff467e0c3250223e4e790c44a6add2b823f2
kashafS <57944227+KashafS98@users.noreply.github.com>
2021-09-25 19:40:36 +0530

    fix: 🐛 responsive ui

--- src/App.js
+++ src/App.js
@@ -7,10 +7,15 @@ function App() {
   
 
   return (
-    <div className="App bg-dark h-screen">
-      <Header/>
-      <Synthesize/>
-      <Footer/>
+    <div className="App bg-dark h-screen ">
+      <div className='lg:block xl:block md:hidden sm:hidden '>
+        <Header/>
+        <Synthesize/>
+        <Footer/>
+      </div>
+      <div className='w-1/2 justify-center items-center text-white mx-auto h-screen lg:hidden xl:hidden md:flex sm:flex  '>
+        I was too excited to publish this to make it mobile responsive, please open your laptops, pcs :)
+      </div>
     </div>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,11 +3,10 @@ import logo from "../assets/logo-dark.png";
 
 export default function Footer() {
   return (
-    <div className="flex justify-between py-5 items-center text-white w-4/5 mx-auto absolute bottom-0 left-10p" >
+    <div className="flex justify-between py-5 px-10 items-center text-white w-full mx-auto bg-dark" >
       <div className='flex items-center justify-evenly w-32'>
         <img src={logo} alt="poetry palette" width='12%' ></img>Poetry Palette
       </div>
-      
       <p>
         Created by <a href="https://wordwarrior.in">WordWarrior</a> using{" "}
         <a href="https://cloud.google.com/text-to-speech/">
